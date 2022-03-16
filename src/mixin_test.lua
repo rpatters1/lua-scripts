c6c55bdde04f3119a766d0b098d28e4e857f8836
Robert Patterson <robert@robertgpatterson.com>
2022-03-16 10:56:20 -0500

    mixin_test

--- src/mixin_test.lua
+++ src/mixin_test.lua
@@ -10,23 +10,8 @@ if finenv.RetainLuaState then
 end
 
 if finenv.IsRGPLua then
-    --require("mobdebug").start()
+    require("mobdebug").start()
 end
-
-if not finaleplugin then
-    print("finaleplugin is nil")
-end
-
-if type(finaleplugin) == "table" then
-    for k, v in pairsbykeys(finaleplugin) do
-        print(tostring(k)..": "..tostring(v))
-    end
-else
-    print("finaleplugin is not a table.")
-end
-
-print("finenv.DebugEnabled: "..tostring(finenv.DebugEnabled))
-print("finenv.LoadedAsString: "..tostring(finenv.LoadedAsString))
 
 if not finenv.RetainLuaState then
     altval = finale.CMDMODKEY_ALT
@@ -38,10 +23,10 @@ if finenv.QueryInvokedModifierKeys and (finenv.QueryInvokedModifierKeys(altval) 
     return
 end
 
---local mixin = require("library.mixin")
-local dialog1 = finale.FCCustomLuaWindow()
+local mixin = require("library.mixin")
+local dialog1 = mixin.FCXCustomLuaWindow()
 print("ClassName of dialog1 is "..dialog1:ClassName())
-local dialog2 = finale.FCCustomLuaWindow()
+local dialog2 = mixin.FCXCustomLuaWindow()
 print("ClassName of dialog2 is "..dialog2:ClassName())
     
 if finenv.IsRGPLua then
